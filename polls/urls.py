5ac7a8e000c6388e60b06a61d54ab1164450e20c
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-05-27 02:25:21 +0100

    Starting app "polls"

---
+++ polls/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    # ex: /polls/
+    path("", views.index, name="index"),
+    # ex: /polls/2/
+    path("<int:question_id>/", views.detail, name="detail"),
+    # ex: /polls/2/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+    # ex: /polls/2/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote"),
+]
