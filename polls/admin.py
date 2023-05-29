f72b2ba25865b8fdf39e890667dac36291435ebe
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-05-29 01:17:45 +0100

    Customizing admin page

--- polls/admin.py
+++ polls/admin.py
@@ -3,4 +3,7 @@ from .models import Question
 
 # Register your models here.
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"]
+
+admin.site.register(Question, QuestionAdmin)
