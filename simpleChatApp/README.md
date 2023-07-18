6d72992475a3090013edb33720d911586702bdde
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-07-18 04:59:53 +0100

    Simple chatApp

---
+++ simpleChatApp/README.md
@@ -0,0 +1,34 @@
+# Simple ChatApp with Django
+This is a simple ChatApp project built with Django, allowing users to create chat rooms and communicate with each other in real-time.
+
+## Features
+* User authentication and authorization  
+* Creation of chat rooms  
+* Real-time messaging between users in a chat room  
+* User-friendly interface  
+
+## Installation
+1. Clone the repository  
+`git clone https://github.com/Leo-Youmbi/starting-with-Django`  
+
+2. Install dependencies  
+`pip install -r requirements.txt`  
+
+3. Copy the .env.example file to .env and fill in the values.  
+`cp .env.example .env`  
+
+4. Apply database migrations  
+`python manage.py migrate`  
+
+5. Run the server  
+`python manage.py runserver`  
+
+6. Access the app by navigating to http://localhost:8000 ↗ in your web browser.  
+
+## Usage
+1. Register for an account or log in if you already have one
+2. Create a new chat room or join an existing one
+3. Start messaging with other users in the chat room in real-time  
+
+## Contributing
+Contributions are always welcome! If you have any bug reports, feature requests, or pull requests, please feel free to submit them.
---
+++ simpleChatApp/chatApp/consumers.py
@@ -0,0 +1,41 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.roomGroupName = "group_chat_gfg"
+        # Join room group
+        await self.channel_layer.group_add(
+            self.roomGroupName,
+            self.channel_name
+        )
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # Leave room group
+        await self.channel_layer.group_discard(
+            self.roomGroupName,
+            self.channel_layer
+        )
+
+    # Receive message from WebSocket
+    async def receive(self, text_data=None):
+        text_data_json = json.loads(text_data)
+        message = text_data_json["message"]
+        username = text_data_json["username"]
+        # Send message to room group
+        await self.channel_layer.group_send(
+            self.roomGroupName, {
+                "type": "sendMessage",
+                "message": message,
+                "username": username
+            }
+        )
+
+    # Receive message from room group
+    async def sendMessage(self, event):
+        message = event["message"]
+        username = event["username"]
+
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({"message": message, "username": username}))
